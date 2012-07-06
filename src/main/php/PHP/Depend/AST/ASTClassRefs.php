a0f13d9e43b89e93ee26de84fd86e4e6e3d64a26
Manuel Pichler <mapi@manuel-pichler.de>
2012-07-06 15:44:43 +0200

    Missing doc comments added to ASTClassRef.

--- src/main/php/PHP/Depend/AST/ASTClassRefs.php
+++ src/main/php/PHP/Depend/AST/ASTClassRefs.php
@@ -1,9 +1,64 @@
 <?php
+/**
+ * This file is part of PHP_Depend.
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2008-2012, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  QualityAssurance
+ * @author    Manuel Pichler <mapi@pdepend.org>
+ * @copyright 2008-2012 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://pdepend.org/
+ */
 
 namespace PHP\Depend\AST;
 
 use \PHP_Depend_Context;
 
+/**
+ * Container holding nodes referenced by a class.
+ *
+ * @category  QualityAssurance
+ * @author    Manuel Pichler <mapi@pdepend.org>
+ * @copyright 2008-2012 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://pdepend.org/
+ * @since     2.0.0
+ */
 class ASTClassRefs
 {
     /**
@@ -34,7 +89,12 @@ class ASTClassRefs
      * @param string $parentClassId
      * @param string[] $implementedInterfaceIds
      */
-    public function __construct(PHP_Depend_Context $context, $namespaceId, $parentClassId, array $implementedInterfaceIds)
+    public function __construct(
+        PHP_Depend_Context $context,
+        $namespaceId,
+        $parentClassId,
+        array $implementedInterfaceIds
+    )
     {
         $this->context                 = $context;
         $this->namespaceId             = $namespaceId;
@@ -42,14 +102,22 @@ class ASTClassRefs
         $this->implementedInterfaceIds = $implementedInterfaceIds;
     }
 
+    /**
+     * Returns the parent namespace of the context class.
+     *
+     * @return \PHP_Depend_AST_Namespace
+     */
     public function getNamespace()
     {
-        if ($namespace = $this->context->getNamespace($this->namespaceId)) {
-            return $namespace;
-        }
-        // TODO Return dummy namespace
+        return $this->context->getNamespace($this->namespaceId);
     }
 
+    /**
+     * Returns the parent class or <b>NULL</b> if the parent class does not
+     * declare a parent.
+     *
+     * @return null|\PHP\Depend\AST\ASTClass
+     */
     public function getParentClass()
     {
         if (null === $this->parentClassId) {
@@ -59,6 +127,8 @@ class ASTClassRefs
     }
 
     /**
+     * Returns all interfaces that are implemented by the context class.
+     *
      * @return \PHP_Depend_AST_Interface[]
      */
     public function getImplementedInterfaces()
